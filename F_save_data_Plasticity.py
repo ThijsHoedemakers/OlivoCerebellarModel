f7fce83ded556632256c7e08f1427137d772b2c2
Thijs Hoedemakers <t.hoedemakers@outlook.com>
2020-01-16 15:23:58 +0100

    changed the data structure of the variables from .mat to pickle

--- F_save_data_Plasticity.py
+++ F_save_data_Plasticity.py
@@ -17,34 +17,53 @@ param="".join(param)
 spikes="".join(spikes)
 
 ### Plasticity Variables
-PV = Struct()
-PV.a_PC = mon_N_PC_Coupled.a_PC
-PV.a_IO = mon_N_PC_Coupled.a_IO
-PV.delta = mon_N_PC_Coupled.delta_weight
+#PV = Struct()
+#PV.a_PC = mon_N_PC_Coupled.a_PC
+#PV.a_IO = mon_N_PC_Coupled.a_IO
+#PV.delta = mon_N_PC_Coupled.delta_weight
+PV={'a_PC':mon_N_PC_Coupled.a_PC, 'a_IO':mon_N_PC_Coupled.a_IO, 'delta':mon_N_PC_Coupled.delta_weight}
+#PVdict['a_PC']=mon_N_PC_Coupled.a_PC
 
+with open('PV.pickle', 'wb') as par:
+    pickle.dump(PV, par, pickle.HIGHEST_PROTOCOL)
+    print('Plasticity variables are saved')
 ### Noise input
-Input = Struct()
-Input.I=Noise_extended_statemon.I
-Input.nweight = mon_N_PC_Coupled.new_weight
-
+#Input = Struct()
+#Input.I=Noise_extended_statemon.I
+#Input.nweight = mon_N_PC_Coupled.new_weight
+Input={'I':Noise_extended_statemon.I, 'nweight':mon_N_PC_Coupled.new_weight}
+with open('Input.pickle', 'wb') as inp:
+    pickle.dump(Input, inp, pickle.HIGHEST_PROTOCOL)
+    print('Inputs are saved')
 ### Voltage of Cell
-VoltCell = Struct()
-VoltCell.IOsoma = IO_Statemon_Coupled_STDP.Vs
-VoltCell.IOdend = IO_Statemon_Coupled_STDP.Vd
-VoltCell.PC = PC_Statemon_Coupled_STDP.v
-VoltCell.DCN = DCN_Statemon_Coupled_STDP.v
-
+#VoltCell = Struct()
+#VoltCell.IOsoma = IO_Statemon_Coupled_STDP.Vs
+#VoltCell.IOdend = IO_Statemon_Coupled_STDP.Vd
+#VoltCell.PC = PC_Statemon_Coupled_STDP.v
+#VoltCell.DCN = DCN_Statemon_Coupled_STDP.v
+VoltCell = {'IOsoma':IO_Statemon_Coupled_STDP.Vs, 'IOdend':IO_Statemon_Coupled_STDP.Vd, 
+            'PC':PC_Statemon_Coupled_STDP.v, 'DCN':DCN_Statemon_Coupled_STDP.v}
+with open('VoltCell.pickle', 'wb') as vc:
+    pickle.dump(VoltCell, vc, pickle.HIGHEST_PROTOCOL)
+    print('Voltage Cells are saved')
 ### Spike times
-SpikeTimes = Struct()
-SpikeTimes.PC = list(PC_Spikemon_Coupled_STDP.spike_trains().values())
-SpikeTimes.DCN = list(DCN_Spikemon_Coupled_STDP.spike_trains().values())
-SpikeTimes.IO = list(IO_Spikemon_Coupled_STDP.spike_trains().values())
+#SpikeTimes = Struct()
+#SpikeTimes.PC = list(PC_Spikemon_Coupled_STDP.spike_trains().values())
+#SpikeTimes.DCN = list(DCN_Spikemon_Coupled_STDP.spike_trains().values())
+#SpikeTimes.IO = list(IO_Spikemon_Coupled_STDP.spike_trains().values())
+
+SpikeTimes = {'PC':list(PC_Spikemon_Coupled_STDP.spike_trains().values()),
+              'DCN':list(DCN_Spikemon_Coupled_STDP.spike_trains().values()),
+              'IO':list(IO_Spikemon_Coupled_STDP.spike_trains().values())}
+with open('SpikeTimes.pickle', 'wb') as st:
+    pickle.dump(SpikeTimes, st, pickle.HIGHEST_PROTOCOL)
+    print('Spike Times are saved')
 #print(SpikeTimes.IO)
 #f = open(spikes,"w")
 #f.write( str(SpikeTimes) )
 #f.close() 
 ### 
-sio.savemat(nois, mdict={'Input':Input})
-sio.savemat(volt, mdict={'VoltCell':VoltCell})
-sio.savemat(param, mdict={'PV':PV})
-sio.savemat(spikes, mdict={'SpikeTimes':SpikeTimes})
+#sio.savemat(nois, mdict={'Input':Input})
+#sio.savemat(volt, mdict={'VoltCell':VoltCell})
+#sio.savemat(param, mdict={'PV':PV})
+#sio.savemat(spikes, mdict={'SpikeTimes':SpikeTimes})
