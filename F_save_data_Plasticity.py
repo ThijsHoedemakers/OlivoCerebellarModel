19e3ad180a19746d8fe6d7443da44d9d4ce2d9c2
Thijs Hoedemakers <t.hoedemakers@outlook.com>
2020-01-20 20:55:06 +0100

    rewrite input to make it easily compatible

--- F_save_data_Plasticity.py
+++ F_save_data_Plasticity.py
@@ -7,10 +7,10 @@ param = list(globname)
 spikes = list(globname)
 nois = list(globname)
 #add the new words
-nois.append('AfterSim.mat')
-volt.append('_VoltageCell.mat')
-param.append('_PlasticityVariables.mat')
-spikes.append('_SpikeTimes.mat')
+nois.append('AfterSim.pickle')
+volt.append('_VoltageCell.pickle')
+param.append('_PlasticityVariables.pickle')
+spikes.append('_SpikeTimes.pickle')
 nois="".join(nois)
 volt="".join(volt)
 param="".join(param)
@@ -24,7 +24,7 @@ spikes="".join(spikes)
 PV={'a_PC':mon_N_PC_Coupled.a_PC, 'a_IO':mon_N_PC_Coupled.a_IO, 'delta':mon_N_PC_Coupled.delta_weight}
 #PVdict['a_PC']=mon_N_PC_Coupled.a_PC
 
-with open('PV.pickle', 'wb') as par:
+with open(param, 'wb') as par:
     pickle.dump(PV, par, pickle.HIGHEST_PROTOCOL)
     print('Plasticity variables are saved')
 ### Noise input
@@ -32,7 +32,7 @@ with open('PV.pickle', 'wb') as par:
 #Input.I=Noise_extended_statemon.I
 #Input.nweight = mon_N_PC_Coupled.new_weight
 Input={'I':Noise_extended_statemon.I, 'nweight':mon_N_PC_Coupled.new_weight}
-with open('Input.pickle', 'wb') as inp:
+with open(nois, 'wb') as inp:
     pickle.dump(Input, inp, pickle.HIGHEST_PROTOCOL)
     print('Inputs are saved')
 ### Voltage of Cell
@@ -43,7 +43,7 @@ with open('Input.pickle', 'wb') as inp:
 #VoltCell.DCN = DCN_Statemon_Coupled_STDP.v
 VoltCell = {'IOsoma':IO_Statemon_Coupled_STDP.Vs, 'IOdend':IO_Statemon_Coupled_STDP.Vd, 
             'PC':PC_Statemon_Coupled_STDP.v, 'DCN':DCN_Statemon_Coupled_STDP.v}
-with open('VoltCell.pickle', 'wb') as vc:
+with open(volt, 'wb') as vc:
     pickle.dump(VoltCell, vc, pickle.HIGHEST_PROTOCOL)
     print('Voltage Cells are saved')
 ### Spike times
@@ -55,7 +55,7 @@ with open('VoltCell.pickle', 'wb') as vc:
 SpikeTimes = {'PC':list(PC_Spikemon_Coupled_STDP.spike_trains().values()),
               'DCN':list(DCN_Spikemon_Coupled_STDP.spike_trains().values()),
               'IO':list(IO_Spikemon_Coupled_STDP.spike_trains().values())}
-with open('SpikeTimes.pickle', 'wb') as st:
+with open(spikes, 'wb') as st:
     pickle.dump(SpikeTimes, st, pickle.HIGHEST_PROTOCOL)
     print('Spike Times are saved')
 #print(SpikeTimes.IO)
