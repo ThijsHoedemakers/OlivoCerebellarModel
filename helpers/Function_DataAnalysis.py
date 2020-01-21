baecbbee171ba2632b5a1ad2c8762de2aee16787
Thijs Hoedemakers <t.hoedemakers@outlook.com>
2020-01-21 14:25:21 +0100

    add connectivity plot& fft of traces

--- helpers/Function_DataAnalysis.py
+++ helpers/Function_DataAnalysis.py
@@ -1,5 +1,8 @@
 # Functions usefull for data analysis
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 def Slicing(data, t_start=None, t_end=None):
     
     output={}
@@ -22,3 +25,33 @@ def SlicingSpikes(data, t_start=float("-inf"), t_end=float("inf")):
     
     return output
 
+def Connectivity(cell1,cell2,name_cell1,name_cell2):   
+    cell1nr =  max(cell1)
+    cell2nr = max(cell2)
+    len_cell = len(cell1)
+    data = np.zeros((cell1nr+1, cell2nr+1)) 
+
+    for p in range(0,len_cell) :  
+        data[cell1[p],cell2[p]] = 1
+  
+    xspace = cell2nr/3
+    yspace = cell1nr/2
+    fig, ax = plt.subplots(1, 1,figsize=(xspace,yspace))
+    ax.set_xticks(np.arange(cell1nr))
+    ax.set_xticks(np.arange(cell2nr))
+    title = 'Vertical Index = ',name_cell1,'Horizontal Index = ',name_cell2
+    ax.set_title(title)
+    ax.set_xlabel(name_cell1)
+    ax.set_ylabel(name_cell2)
+    sns.heatmap(data,cmap='Greys',linewidths=.5,linecolor='white',cbar=False,square=True)
+    
+    plt.show()
+def FreqSpectrum(x):
+    spec = np.absolute(np.fft.fft(x))
+    leng = int(len(x)/2)
+    spec = spec[:leng]  # take positive frequency components
+    spec /= len(x)  # normalize
+    spec *= 2.0  # to get amplitudes of sine components, need to multiply by 2
+    spec[0] /= 2.0  # except for the dc component
+    return spec
+    
